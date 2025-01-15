3871fdfdbecde4a2b986524926a998d38036c729
Luana P. Reis <luana154reis@gmail.com>
2025-01-15 17:48:43 -0300

    TS_min = 2 and quality_filters applied

--- 4LAC_variability_analysis/source_filter.py
+++ 4LAC_variability_analysis/source_filter.py
@@ -24,12 +24,76 @@ def generate_list_sources(binning=['3-days', 'weekly', 'monthly'], index=['fixed
 
 
 
+
+
+
+def check_and_fill_bins(source_dataframe):
+    filtered_df = source_dataframe.copy()
+    
+    TS_min = 2
+    
+    ## remove invalid bins
+    indices_ferror = (source_dataframe['flux_error'] == 0) # flux_error == 0
+    indices_fit = (source_dataframe['fit_convergence'] != 0) # fit_convergence != 0
+    
+    indices_to_remove = indices_ferror | indices_fit
+    filtered_df.loc[indices_to_remove, ['flux', 'flux_error']] = np.nan
+    
+    exposure = filtered_df['flux'] / ((filtered_df['flux_error'])**2)
+    indices_expo = (exposure < 1e7) # exposure < 1e7 cm2 s
+    filtered_df.loc[indices_expo, ['flux', 'flux_error']] = np.nan
+    
+    # (np.isnan(srcdf['flux'].loc[idx]) and np.isnan(srcdf['flux_upper_limits'].loc[idx])) or
+    # (np.isnan(srcdf['flux'].loc[idx]) and srcdf['flux_upper_limits'].loc[idx]<0)  or                      
+    # (srcdf['flux_error'].loc[idx] > 1e-12 and exposure < 1e7)
+    
+    
+    
+    ## estimate median of ULs for 1 < TS < TS_min
+    median_df = filtered_df.copy()
+    ts_condition = (median_df['values_ts'] > 1) & (median_df['values_ts'] < TS_min)
+    
+    median_flux_value = median_df.loc[ts_condition, 'flux'].median()
+    # print('median_flux_value: ', median_flux_value)
+    
+    ## remove TS < TS_min (flux & flux_error = np.nan)
+    indices_ts = (median_df['values_ts'] < TS_min) # TS < TS_min
+    median_df.loc[indices_ts, ['flux', 'flux_error']] = np.nan
+    
+    ## estimate total number of invalid bins
+    n_unconstrained = median_df['flux'].isna().sum()
+    
+    ## input invalid bins (NaNs)
+    inputed_df = median_df.copy()
+    
+    ## replace all initial NaN bins in 'flux_error' with the median_flux
+    first_valid_index = inputed_df['flux_error'].first_valid_index()
+    
+    if first_valid_index is not None:
+        ## fill NaNs in 'flux_error' before the first valid index with median_flux
+        inputed_df.loc[:first_valid_index, 'flux_error'] = inputed_df['flux_error'].fillna(median_flux_value)
+    
+    
+    ## replace NaN in flux_error by the previous bin flux (that's not NaN)
+    inputed_df['flux_error'] = inputed_df['flux_error'].fillna(inputed_df['flux'].ffill())
+    
+    indices_invalid = np.isnan(inputed_df['flux']) # remaining NaN values in 'flux'
+    inputed_df.loc[indices_invalid, 'flux'] = 0  # Input zero
+    
+    
+    
+    return filtered_df, median_df, inputed_df, n_unconstrained
+
+
+
+
+
 def filter_outliers(source_dataframe):
     filtered_df = source_dataframe.copy()
     
     # print('# of bins:', len(source_dataframe))
     
-    exposure = source_dataframe['flux'] / (source_dataframe['flux_error'] ** 2)
+    exposure = source_dataframe['flux'] / ((source_dataframe['flux_error'])**2)
     
     ## to remove
     indices_ts = (source_dataframe['values_ts'] < 4) # TS < 4
@@ -48,13 +112,12 @@ def filter_outliers(source_dataframe):
 
 
 
-
-
 def input_upperL(filtered_df, case=['average', 'zero']):
     inputed_df = filtered_df.copy()
 
+    ## wrong - fix later
     ## bins where 1<TS<2
-    ts_condition = (filtered_df['values_ts'] > 1) & (filtered_df['values_ts'] < 2)
+    ts_condition = (filtered_df['values_ts'] > 1) & (filtered_df['values_ts'] < 4)
     
     ## median of 'flux' for bins satisfying the TS condition
     median_flux = filtered_df.loc[ts_condition, 'flux'].median()
